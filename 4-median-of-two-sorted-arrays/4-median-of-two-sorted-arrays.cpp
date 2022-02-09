1560fa642c83c31f6354b4f43a16dbc6fc23ec24
Sumandeep Kaur <77191007+Sumandeep-Kaur@users.noreply.github.com>
2022-02-09 16:17:52 +0530

    Time: 32 ms (79.61%), Space: 89.4 MB (60.96%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> v;
-        int i = 0, j = 0;
-        while(i < nums1.size() && j < nums2.size()) {
-            if(nums1[i] < nums2[j]) {
-                v.push_back(nums1[i]);
-                i++;
+        int m = nums1.size(), n = nums2.size();
+        
+        if(m > n)
+            return findMedianSortedArrays(nums2, nums1);
+
+        int low = 0, high = m, medianPos = ((m+n)+1)/2;
+        
+        while(low <= high) {
+            int cut1 = (low + high)/2;
+            int cut2 = medianPos - cut1;
+
+            int l1 = (cut1 == 0)? INT_MIN : nums1[cut1-1];
+            int l2 = (cut2 == 0)? INT_MIN : nums2[cut2-1];
+            int r1 = (cut1 == m)? INT_MAX : nums1[cut1];
+            int r2 = (cut2 == n)? INT_MAX : nums2[cut2];
+
+            if(l1 <= r2 && l2 <= r1) {
+                if((m+n) % 2 != 0)
+                    return max(l1,l2);
+                else 
+                    return (max(l1,l2) + min(r1,r2))/2.0;
             }
-            else {
-                v.push_back(nums2[j]);
-                j++;
-            }
+            else if(l1 > r2) 
+                high = cut1-1;
+            else 
+                low = cut1+1;
         }
-        if(i < nums1.size()) {
-            while(i < nums1.size()) { 
-                v.push_back(nums1[i]);
-                i++;
-            }
-        }
-        if(j < nums2.size()) {
-            while(j < nums2.size()) {
-                v.push_back(nums2[j]);
-                j++;
-            }
-        }
-        int n = nums1.size() + nums2.size();
-        if(n % 2 == 1) 
-            return v[((n+1)/2)-1];
-        else 
-            return (v[n/2-1] + v[n/2])/2.0;   
+        return 0.0;  
     }
 };

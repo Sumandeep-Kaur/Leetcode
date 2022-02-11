0b54461ca2b542a7effd99d45ec0e7cff21ae075
Sumandeep Kaur <77191007+Sumandeep-Kaur@users.noreply.github.com>
2022-02-11 18:50:51 +0530

    Time: 4 ms (94.04%), Space: 9 MB (82.4%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -2,34 +2,28 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         stack<int> st;
-        vector<vector<int>> v;
-        for(int i = nums2.size() - 1; i >= 0; i--) {
-            if(st.empty()) { 
-                v.push_back({nums2[i], -1});
-            }
-            else if(st.size() > 0 && st.top() >= nums2[i]) {
-                v.push_back({nums2[i], st.top()});
-            }
-            else {
-                while(st.size() > 0 && st.top() < nums2[i])
+        map<int, int> mp;
+        
+        for(int i = nums2.size() - 1; i >= 0; i--){
+            if(st.size() == 0)
+                mp[nums2[i]] = -1;
+            else if(st.size() > 0 && st.top() > nums2[i])
+                mp[nums2[i]] = st.top();
+            else if(st.size() > 0 && st.top() <= nums2[i]) {
+                while(st.size() > 0 && st.top() <= nums2[i])
                     st.pop();
-                if(st.empty()) 
-                    v.push_back({nums2[i], -1});
-                else 
-                    v.push_back({nums2[i], st.top()});
+                
+                if(st.size() == 0)
+                    mp[nums2[i]] = -1;
+                else
+                    mp[nums2[i]] = st.top();
             }
             st.push(nums2[i]);
         }
         
         vector<int> ans;
-        for(int i = 0; i < nums1.size(); i++) {
-            for(int j = 0; j < v.size(); j++) {
-                if(v[j][0] == nums1[i]) { 
-                    ans.push_back(v[j][1]);
-                    break;
-                }
-            }
-        }
+        for(int i = 0; i < nums1.size(); i++)
+            ans.push_back(mp[nums1[i]]);
         
         return ans;
     }

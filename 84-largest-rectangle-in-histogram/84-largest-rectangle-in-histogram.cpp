eda5b6944e054a01ce084b5e546acd805d3e3e64
Sumandeep Kaur <77191007+Sumandeep-Kaur@users.noreply.github.com>
2022-02-13 17:29:13 +0530

    Time: 124 ms (85.75%), Space: 75.4 MB (9.62%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,55 +1,23 @@
 class Solution {
 public:
-    vector<int> nextSmallerToLeft(vector<int>& heights) {
-        vector<int> v;
-        stack<pair<int, int>> st;
-        for(int i = 0; i < heights.size(); i++) {
-            if(st.size() == 0)
-                v.push_back(-1);
-            else if(st.size() > 0 && st.top().first < heights[i])
-                v.push_back(st.top().second);
-            else if(st.size() > 0 && st.top().first >= heights[i]) {
-                while(st.size() > 0 && st.top().first >= heights[i])
-                    st.pop();
-                if(st.size() == 0)
-                    v.push_back(-1);
-                else
-                    v.push_back(st.top().second);
-            }
-            st.push({heights[i], i});
-        }
-        return v;
-    }
-    
-    vector<int> nextSmallerToRight(vector<int>& heights) {
-        vector<int> v;
-        stack<pair<int, int>> st;
-        for(int i = heights.size() - 1; i >= 0; i--) {
-            if(st.size() == 0)
-                v.push_back(heights.size());
-            else if(st.size() > 0 && st.top().first < heights[i])
-                v.push_back(st.top().second);
-            else if(st.size() > 0 && st.top().first >= heights[i]) {
-                while(st.size() > 0 && st.top().first >= heights[i])
-                    st.pop();
-                if(st.size() == 0)
-                    v.push_back(heights.size());
-                else
-                    v.push_back(st.top().second);
-            }
-            st.push({heights[i], i});
-        }
-        reverse(v.begin(), v.end());
-        return v;
-    }
-    
     int largestRectangleArea(vector<int>& heights) {
-        vector<int> left = nextSmallerToLeft(heights);
-        vector<int> right = nextSmallerToRight(heights);
-        
+        stack < int > st;
         int area = 0;
-        for(int i = 0; i < heights.size(); i++) { 
-            area = max(area, heights[i] * (right[i] - left[i] - 1));
+        int n = heights.size();
+        
+        for (int i = 0; i <= n; i++) {
+            while (!st.empty() && (i == n || heights[st.top()] >= heights[i])) {
+                int height = heights[st.top()];
+                st.pop();
+                int width;
+                if (st.empty())
+                    width = i;
+                else
+                    width = i - st.top() - 1;
+                
+                area = max(area, height * width);
+            }
+            st.push(i);
         }
         
         return area;

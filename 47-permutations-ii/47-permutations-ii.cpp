ee24e8803eb3bba31af45d7f0f58878ceb859aee
Sumandeep Kaur <77191007+Sumandeep-Kaur@users.noreply.github.com>
2022-02-27 21:58:29 +0530

    Time: 11 ms (64.49%), Space: 9.4 MB (55.86%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -3,6 +3,7 @@ public:
     void permutations(vector<int> nums, int i, vector<vector<int>>& ans) {
         if(i == nums.size()) {
             ans.push_back(nums);
+            return;
         }    
         for(int j = i; j < nums.size(); j++) {
             if(j == i || nums[j] != nums[i]) {

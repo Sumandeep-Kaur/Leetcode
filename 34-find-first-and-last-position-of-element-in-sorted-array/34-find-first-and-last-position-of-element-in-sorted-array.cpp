0c6a664b6a62db42da39263157e668ac0b64feef
Sumandeep Kaur <77191007+Sumandeep-Kaur@users.noreply.github.com>
2022-03-04 19:40:24 +0530

    Time: 13 ms (41.00%), Space: 13.7 MB (19.35%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,43 +1,33 @@
 class Solution {
 public:
-    int firstOccurence(vector<int>& nums, int low, int high, int target) {
-        int first = -1;
-        while(low <= high) {
-            int mid = (low + high)/2;
-            if(target == nums[mid]) {
-                first = mid;
-                high = mid - 1;
-            }
-            else if(target < nums[mid])
-                high = mid - 1;
-            else
-                low = mid + 1;
-        }
-        return first;
-    }
-    
-    int lastOccurence(vector<int>& nums, int low, int high, int target) {
-        int last = -1;
-        while(low <= high) {
-            int mid = (low + high)/2;
-            if(target == nums[mid]) {
-                last = mid;
-                low = mid + 1;
-            }
-            else if(target < nums[mid])
-                high = mid - 1;
-            else
-                low = mid + 1;
-        }
-        return last;
-    }
-    
     vector<int> searchRange(vector<int>& nums, int target) {
-        int low = 0, high = nums.size() - 1;
-        int first = firstOccurence(nums, low, high, target);
-        int last = lastOccurence(nums, low, high, target);
+        vector<int> res(2, -1);
+        if(nums.size() == 0)
+            return res;
         
-        vector<int> res = {first, last};
-        return res;
+        int low = 0, high = nums.size() - 1;
+        while(low < high) {
+            int mid = (low + high)/2;
+            if (target > nums[mid]) 
+                low = mid + 1;
+            else 
+                high = mid;
+        }
+        if (nums[low] != target) 
+            return res;
+        else 
+            res[0] = low;
+
+        high = nums.size() - 1;
+        while(low < high) {
+            int mid = (low + high)/2 + 1;	
+            if (target < nums[mid]) 
+                high = mid - 1;  
+            else 
+                low = mid;				
+        }
+        res[1] = high;
+        
+        return res; 
     }
 };

1645ce393c98ff2afed0bbebe1a2ba48f125e692
Sumandeep Kaur <77191007+Sumandeep-Kaur@users.noreply.github.com>
2022-03-06 15:17:34 +0530

    Time: 0 ms (100.00%), Space: 6 MB (53.14%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
-    long totalWays(int unpicked, int undelivered, vector<vector<int>>& dp) {
-        if(unpicked == 0 && undelivered == 0)
-            return 1;
-        if(unpicked < 0 || undelivered < 0 || undelivered < unpicked)
-            return 0;
-        
-        if(dp[unpicked][undelivered] != 0)
-            return dp[unpicked][undelivered];
-        
-        long ans = 0, mod = 1e9 + 7;
-        ans += unpicked * totalWays(unpicked - 1, undelivered, dp);
-        ans += (undelivered - unpicked) * totalWays(unpicked, undelivered - 1, dp);
-        ans %= mod;
-        
-        return dp[unpicked][undelivered] = ans;
-    }
     int countOrders(int n) {
-        vector<vector<int>> dp(n + 1, vector<int> (n + 1, 0));
-        return totalWays(n, n, dp);
+        long ans = 1, mod = 1e9 + 7;
+        
+        for(int i = 1; i <= n; i++) {
+            // Ways to arrange all n pickup = n! and for n deliveries = (2n-1)
+            ans *= i * (2*i - 1);
+            ans %= mod;
+        }
+        
+        return ans;
     }
 };

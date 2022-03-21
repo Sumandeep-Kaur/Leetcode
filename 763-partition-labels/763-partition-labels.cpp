af398fe3119e87f3642f9f6db06969625ecb6371
Sumandeep Kaur <77191007+Sumandeep-Kaur@users.noreply.github.com>
2022-03-21 18:52:30 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (96.62%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        vector<int> last(26, -1);
+        for(int i = 0; i < s.length(); i++) {
+            last[s[i] - 'a'] = i;
+        }
+        
+        int start = 0, end = 0;
+        vector<int> ans;
+        for(int i = 0; i < s.length(); i++) {
+            end = max(end, last[s[i] - 'a']);
+            if(i == end) {
+                ans.push_back(end - start + 1);
+                start = i + 1;
+            }
+        }
+        
+        return ans;
+    }
+};

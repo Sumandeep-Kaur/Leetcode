34eaa627b5506d9713b3147a4d3514d3b5ffc9a3
Sumandeep Kaur <77191007+Sumandeep-Kaur@users.noreply.github.com>
2022-03-27 12:40:11 +0530

    Time: 27 ms (13.19%), Space: 10.3 MB (91.37%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
-        vector<pair<int, int>> soldiers;
+        priority_queue<pair<int, int>> maxHeap;
         
         for(int i = 0; i < mat.size(); i++) {
-            int count = 0;
-            for(int j = 0; j < mat[0].size(); j++) {
-                if(mat[i][j] == 1)
-                    count++;
+            int l = 0, h = mat[0].size() - 1;
+            while(l <= h) {
+                int mid = (l + h) / 2;
+                if(mat[i][mid] == 0)
+                    h = mid - 1;
+                else
+                    l = mid + 1;
             }
-            soldiers.push_back({count, i});
+            
+            maxHeap.push({l, i});
+            
+            if(maxHeap.size() > k)
+                maxHeap.pop();
         }
-        
-        sort(soldiers.begin(), soldiers.end());
         
         vector<int> ans;
-        for(int i = 0; i < k; i++) {
-            ans.push_back(soldiers[i].second);
+        while(maxHeap.size() > 0) {
+            ans.push_back(maxHeap.top().second);
+            maxHeap.pop();
         }
         
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };

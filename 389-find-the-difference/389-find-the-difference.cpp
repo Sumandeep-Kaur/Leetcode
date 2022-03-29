280c4cc90aab470c819b67c13ded7227da44bd07
Sumandeep Kaur <77191007+Sumandeep-Kaur@users.noreply.github.com>
2022-03-29 17:16:58 +0530

    Time: 7 ms (26.98%), Space: 6.6 MB (66.59%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int ans = 0; 
+        for(int i = 0; i < s.length(); i++) {
+            ans ^= (s[i] - 'a');
+            ans ^= (t[i] - 'a');
+        }
+        ans ^= t[t.length() - 1] - 'a';
+        return (ans + 'a');
+    }
+};

74a0f04ce50a2337cea1c81cadf6dab73071e04f
Sumandeep Kaur <77191007+Sumandeep-Kaur@users.noreply.github.com>
2022-04-11 18:13:09 +0530

    Time: 40 ms (53.56%), Space: 13.7 MB (97.44%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -7,15 +7,12 @@ public:
             return grid;
         
         while(k--) {
-            int first = grid[m - 1][n - 1];
-            for(int i = m - 1; i >= 0; i--) {
-                for(int j = n - 1; j > 0; j--) {
-                    grid[i][j] = grid[i][j - 1];
+            int prev = grid[m - 1][n - 1];
+            for(int i = 0; i < m; i++) {
+                for(int j = 0; j < n; j++) {
+                    swap(grid[i][j], prev);
                 }
-                if(i - 1 >= 0)
-                    grid[i][0] = grid[i - 1][n - 1];
             }
-            grid[0][0] = first;
         }
         
         return grid;

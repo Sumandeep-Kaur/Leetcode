842a79c2d79993b4ce63a2395ab723616f4c4fe0
Sumandeep Kaur <77191007+Sumandeep-Kaur@users.noreply.github.com>
2022-04-12 18:33:00 +0530

    Time: 4 ms (26.80%), Space: 6.9 MB (58.55%) - LeetHub

---
+++ 289-game-of-life/289-game-of-life.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void gameOfLife(vector<vector<int>>& board) {
+        int n = board.size(), m = board[0].size();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                int count = 0;
+                if(i > 0 && j > 0 && board[i-1][j-1] >= 1) count++; //top left
+                if(j > 0 && board[i][j-1] >= 1) count++; //top
+                if(i < n-1 && j > 0 && board[i+1][j-1] >= 1) count++; //top right
+                if(i > 0 && board[i-1][j] >= 1) count++; //left
+                if(i < n-1 && board[i+1][j] >= 1) count++; //right
+                if(i > 0 && j < m-1 && board[i-1][j+1] >= 1) count++; //down left
+                if(j < m-1 && board[i][j+1] >= 1) count++; //down
+                if(i < n-1 && j < m-1 && board[i+1][j+1] >= 1) count++; //down right
+
+                if(board[i][j] == 1 && (count < 2 || count > 3)) 
+                    board[i][j] = 2;   //make it dead
+                if(board[i][j] == 0 && count == 3) 
+                    board[i][j] = -2;   //make it live
+            }
+        }
+        
+        for(int i = 0; i < board.size(); i++){
+            for(int j = 0; j < board[0].size(); j++){
+                if(board[i][j] == 2) 
+                    board[i][j] = 0;
+                if(board[i][j] == -2) 
+                    board[i][j] = 1;
+            }
+        }
+    }
+};

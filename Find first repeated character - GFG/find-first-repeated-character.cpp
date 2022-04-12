f1697a29570879f5cb44ef3b9c5f48f236e64c4d
Sumandeep Kaur <77191007+Sumandeep-Kaur@users.noreply.github.com>
2022-04-12 20:31:09 +0530

    Added solution - LeetHub

---
+++ Find first repeated character - GFG/find-first-repeated-character.cpp
@@ -0,0 +1,30 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+string firstRepChar(string s);
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        cout<<firstRepChar(s)<<endl;
+    }
+	return 0;
+}// } Driver Code Ends
+
+
+string firstRepChar(string s)
+{
+    unordered_map<char, int> mp;
+    for(int i = 0; i < s.length(); i++) {
+        mp[s[i]]++;
+        if(mp[s[i]] == 2) { 
+            string ans(1, s[i]);
+            return ans;
+        }
+    }
+    return "-1";
+}
